ca359c8506bc8b9180dec5544590842f34c26994
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-11 07:52:50 +0100

    Kiinduló állapot

---
+++ Kreata.Backend/Controllers/StudentController.cs
@@ -0,0 +1,67 @@
+﻿using Kreata.Backend.Repos;
+using Kreta.Shared.Dtos;
+using Kreta.Shared.Extensions;
+using Kreta.Shared.Models;
+using Kreta.Shared.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kreata.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private IStudentRepo _studentRepo;
+
+        public StudentController(IStudentRepo studentRepo)
+        {
+            _studentRepo = studentRepo;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBy(Guid id)
+        {
+            Student? entity = new();
+            if (_studentRepo is not null)
+            {
+                entity = await _studentRepo.GetBy(id);
+                if (entity!=null) 
+                    return Ok(entity.ToStudentDto());
+            }
+            return BadRequest("Az adatok elérhetetlenek!");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SelectAllRecordToListAsync()
+        {
+            List<Student>? users = new();
+
+            if (_studentRepo != null)
+            {
+                users = await _studentRepo.GetAll();
+                return Ok(users.Select(student => student.ToStudentDto()));
+            }
+            return BadRequest("Az adatok elérhetetlenek!");
+        }
+
+        [HttpPut()]
+        public async Task<ActionResult> UpdateStudentAsync(StudentDto entity)
+        {
+            ControllerResponse response = new();
+            if (_studentRepo is not null)
+            {
+                response = await _studentRepo.UpdateStudentAsync(entity.ToStudent());
+                if (response.HasError)
+                {
+                    return BadRequest(response);
+                }
+                else
+                {
+                    return Ok(response);
+                }
+            }
+            response.ClearAndAddError("Az adatok frissítés nem lehetséges!");
+            return BadRequest(response);
+        }
+    }
+}
---
+++ Kreata.Backend/Repos/IStudentRepo.cs
@@ -0,0 +1,12 @@
+﻿using Kreta.Shared.Models;
+using Kreta.Shared.Responses;
+
+namespace Kreata.Backend.Repos
+{
+    public interface IStudentRepo
+    {
+        Task<List<Student>> GetAll();
+        Task<Student?> GetBy(Guid id);
+        Task<ControllerResponse> UpdateStudentAsync(Student student);
+    }
+}
