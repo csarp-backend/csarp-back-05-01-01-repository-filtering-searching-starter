3a407d8f42c1a4c4e233d39a2890bb496e95f022
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-24 09:32:24 +0100

    Repository minta osztály

---
+++ Kreata.Backend/Repos/RepositoryBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kreata.Backend.Repos
+{
+    public class RepositoryBase<TDbContext, TEntity> : IRepositoryBase<TEntity>
+        where TDbContext : DbContext
+        where TEntity : class, new()
+
+    {
+    }
+}
